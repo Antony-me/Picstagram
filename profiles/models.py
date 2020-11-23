4471ddbcce6baf23815c79283c24c1255274db53
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-23 13:24:37 +0300

    Added Relationship class

--- profiles/models.py
+++ profiles/models.py
@@ -42,4 +42,18 @@ class Profile(models.Model):
             self.slug = to_slug
             super().save(*args, **kwargs)
 
-    
+
+STATUS_CHOICES = (
+    ('send', 'send'),
+    ('accepted', 'accepted')
+)
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.status}"
+        
