b26453fafd3b4cb5ceb74d74bd15344e3b67e271
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-23 22:44:38 +0300

    Added update profile form

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 from django.http import request
 from django.shortcuts import render
 from .models import Profile
+from .forms import ProfileModelForm
 
 
 def my_profile_view(request):
 
     profile = Profile.objects.get(user=request.user)
+    form = ProfileModelForm()
    
-    return render(request,'profiles/myprofile.html', {'profile':profile})
+    return render(request,'profiles/myprofile.html', {'profile':profile, 'form':form})
