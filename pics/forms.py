2a00a95d56d83c3020e00ad7d412622fdef4ae18
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-24 08:25:57 +0300

    Added new comment form

--- pics/forms.py
+++ pics/forms.py
@@ -7,3 +7,10 @@ class PostModelForm(forms.ModelForm):
     class Meta:
         model= Post
         fields=('content', 'image' )
+
+
+class CommentModelForm(forms.ModelForm):
+    body = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Add a comment...'}))
+    class Meta:
+        model = Comment
+        fields = ('body',)
