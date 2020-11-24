1d9d515645adc3f83eb219c5a58c9d9802ecb129
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-24 22:07:13 +0300

    Added static-root path to urls

--- picstagram/urls.py
+++ picstagram/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,6 +30,6 @@ urlpatterns = [
     path(r'',include('pics.urls', namespace='pics')),
     path('accounts/', include('profiles.urls', namespace='profiles')),
     path(r'^tinymce/', include('tinymce.urls')),
-    
-    
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
