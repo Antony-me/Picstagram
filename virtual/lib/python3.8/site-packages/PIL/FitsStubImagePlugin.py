55995a9665c7945e4a1b03c1feed57f5a25e8bab
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-25 00:36:59 +0300

    Fixed requirements.txt for deployment

--- virtual/lib/python3.8/site-packages/PIL/FitsStubImagePlugin.py
+++ virtual/lib/python3.8/site-packages/PIL/FitsStubImagePlugin.py
@@ -9,24 +9,28 @@
 # See the README file for information on usage and redistribution.
 #
 
-from PIL import Image, ImageFile
+from . import Image, ImageFile
 
 _handler = None
 
-##
-# Install application-specific FITS image handler.
-#
-# @param handler Handler object.
 
 def register_handler(handler):
+    """
+    Install application-specific FITS image handler.
+
+    :param handler: Handler object.
+    """
     global _handler
     _handler = handler
+
 
 # --------------------------------------------------------------------
 # Image adapter
 
+
 def _accept(prefix):
     return prefix[:6] == b"SIMPLE"
+
 
 class FITSStubImageFile(ImageFile.StubImageFile):
 
@@ -47,7 +51,7 @@ class FITSStubImageFile(ImageFile.StubImageFile):
 
         # make something up
         self.mode = "F"
-        self.size = 1, 1
+        self._size = 1, 1
 
         loader = self._load()
         if loader:
@@ -59,7 +63,7 @@ class FITSStubImageFile(ImageFile.StubImageFile):
 
 def _save(im, fp, filename):
     if _handler is None or not hasattr("_handler", "save"):
-        raise IOError("FITS save handler not installed")
+        raise OSError("FITS save handler not installed")
     _handler.save(im, fp, filename)
 
 
@@ -69,5 +73,4 @@ def _save(im, fp, filename):
 Image.register_open(FITSStubImageFile.format, FITSStubImageFile, _accept)
 Image.register_save(FITSStubImageFile.format, _save)
 
-Image.register_extension(FITSStubImageFile.format, ".fit")
-Image.register_extension(FITSStubImageFile.format, ".fits")
+Image.register_extensions(FITSStubImageFile.format, [".fit", ".fits"])
