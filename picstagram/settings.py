91eeb36a9e8e0f3773c6851f6807ac7b48948206
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-25 00:27:36 +0300

    fixed settings registration

--- picstagram/settings.py
+++ picstagram/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = ['picstagram420.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'registration',
     'cloudinary',
     'bootstrap4',
     'pics.apps.PicsConfig',
