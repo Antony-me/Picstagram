29b95c8968adf324982643f511524b54c28466fb
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-24 06:44:06 +0300

    Added coments ad likes models

--- pics/models.py
+++ pics/models.py
@@ -22,3 +22,31 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-created',)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField(max_length=300)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.pk)
+
+LIKE_CHOICES = (
+    ('Like', 'Like'),
+    ('Unlike', 'Unlike'),
+)
+
+class Like(models.Model): 
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES, max_length=8)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user}-{self.post}-{self.value}"
+
+    
