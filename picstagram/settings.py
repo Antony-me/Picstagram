56d5c7f21e085ef6a32a1bcf3c3709d2cc613f32
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-25 10:56:48 +0300

    deployment

--- picstagram/settings.py
+++ picstagram/settings.py
@@ -45,14 +45,11 @@ INSTALLED_APPS = [
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
-SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
